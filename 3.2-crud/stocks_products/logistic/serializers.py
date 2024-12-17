6ade9d0cdf7c2161ec8bac14c898821b28b261d9
adlikovskii <149791077+adlikovskii@users.noreply.github.com>
2024-12-17 18:24:14 +0300

    Add files via upload

---
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # заполняем связанную таблицу StockProduct
+        for position in positions:
+            StockProduct.objects.get_or_create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # обновляем связанную таблицу StockProduct
+        for product in positions:
+            StockProduct.objects.update_or_create(defaults={'quantity': product['quantity'],
+                                                            'price': product['price']},
+                                                  product=product['product'],
+                                                  stock=stock)
+
+        return stock
